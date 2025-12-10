164b7df5da306c3587b6f66de58cb53992b463da
nickkcj <nickcj123@hotmail.com>
2025-12-10 13:22:09 -0300

    feat: add comments module exports

---
+++ src/components/comments/index.ts
@@ -0,0 +1,5 @@
+export { CommentForm } from './comment-form';
+export { CommentItem } from './comment-item';
+export { CommentThread } from './comment-thread';
+export { CommentPreview } from './comment-preview';
+export { CommentSkeleton, CommentListSkeleton } from './comment-skeleton';
